7b31cb6820f8cd17dfab4f464397e681fd7b789d
maxpilotto <maxpil99@outlook.com>
2021-05-27 15:42:06 +0200

    Replace all Set<> return type with List<>

--- krand-core/src/main/kotlin/com/maxpilotto/krand/extensions/Array.kt
+++ krand-core/src/main/kotlin/com/maxpilotto/krand/extensions/Array.kt
@@ -6,14 +6,14 @@ fun <T> Array<T>.pickOne(seed: Any? = null): T {
     return Pick.one(this, seed)
 }
 
-fun <T> Array<T>.pickSet(count: Int, seed: Any? = null): Set<T> {
-    return Pick.set(this, count, seed)
+fun <T> Array<T>.pickMany(count: Int, seed: Any? = null): List<T> {
+    return Pick.many(this, count, seed)
 }
 
 fun <T> Array<T>.pickWeighted(weights: Iterable<Int>, seed: Any? = null): T {
     return Pick.weighted(this, weights, seed)
 }
 
-fun <T> Array<T>.pickWeightedSet(weights: Iterable<Int>, count: Int, seed: Any? = null): Set<T> {
-    return Pick.weightedSet(this, weights, count, seed)
+fun <T> Array<T>.pickWeighted(weights: Iterable<Int>, count: Int, seed: Any? = null): List<T> {
+    return Pick.weighted(this, weights, count, seed)
 }
--- krand-core/src/main/kotlin/com/maxpilotto/krand/extensions/Iterable.kt
+++ krand-core/src/main/kotlin/com/maxpilotto/krand/extensions/Iterable.kt
@@ -6,14 +6,14 @@ fun <T> Iterable<T>.pickOne(seed: Any? = null): T {
     return Pick.one(this, seed)
 }
 
-fun <T> Iterable<T>.pickSet(count: Int, seed: Any? = null): Set<T> {
-    return Pick.set(this, count, seed)
+fun <T> Iterable<T>.pickMany(count: Int, seed: Any? = null): List<T> {
+    return Pick.many(this, count, seed)
 }
 
 fun <T> Iterable<T>.pickWeighted(weights: Iterable<Int>, seed: Any? = null): T {
     return Pick.weighted(this, weights, seed)
 }
 
-fun <T> Iterable<T>.pickWeightedSet(weights: Iterable<Int>, count: Int, seed: Any? = null): Set<T> {
-    return Pick.weightedSet(this, weights, count, seed)
+fun <T> Iterable<T>.pickWeighted(weights: Iterable<Int>, count: Int, seed: Any? = null): List<T> {
+    return Pick.weighted(this, weights, count, seed)
 }
